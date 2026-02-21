fc21b0a8729d9610b78bc4e76062344ab57968aa
Aaryan <aryandhiman540@gmail.com>
2026-02-21 18:58:22 +0530

    Program to find the factorial

---
+++ Functions/factorial_calculator.py
@@ -0,0 +1,10 @@
+def factorial(num):
+    fact = 1
+
+    for i in range (1 , num+1):
+        fact = fact * i
+    return fact
+
+num = int(input("Enter the  number here :"))
+result = factorial(num)
+print("Factorial is " ,result)
